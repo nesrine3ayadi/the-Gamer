b07dd3737efb8820b309e7033ba46f31207ae902
nesrine3ayadi <nesrine3ayadi@gmail.com>
2020-05-13 00:51:05 +0100

    updated profile

--- frontend/src/Components/Profile/profile.jsx
+++ frontend/src/Components/Profile/profile.jsx
@@ -27,47 +27,47 @@ const Profile =(props)=> {
   return (
 
    
-    <div class="single-channel-page" id="content-wrapper">
+    <div className="single-channel-page" id="content-wrapper">
       <Navbar />
-      <div class="single-channel-image">
-        <img class="img-fluid" alt="" src={channelbanner} />
-        <div class="channel-profile">
-          <img class="channel-profile-img" alt="" src={s2} />
+      <div className="single-channel-image">
+        <img className="img-fluid" alt="" src={channelbanner} />
+        <div className="channel-profile">
+          <img className="channel-profile-img" alt="" src={s2} />
        
         </div>
       </div>
-      <div className="row"> 
+      <div classNameName="row"> 
 
       
-      <FormControl className="col-md-8">
+      <FormControl classNameName="col-md-8">
         <InputLabel htmlFor="usename">Username</InputLabel>
         <Input id="username" aria-describedby="my-helper-text"  value ={props.currentUser.username}/>
         <FormHelperText id="my-helper-text">
           Change your username.
         </FormHelperText>
       </FormControl>
-      <FormControl className="col-md-8">
+      <FormControl classNameName="col-md-8">
         <InputLabel htmlFor="Email">Email address</InputLabel>
         <Input id="Email" aria-describedby="my-helper-text"  value={props.currentUser.email} />
         <FormHelperText id="my-helper-text">
          Put your email.
         </FormHelperText>
       </FormControl>
-      <FormControl className="col-md-8">
+      <FormControl classNameName="col-md-8">
         <InputLabel htmlFor="Aboutuser">About me</InputLabel>
         <Input id="Aboutuser" aria-describedby="my-helper-text" value={props.currentUser.aboutUser} />
         <FormHelperText id="my-helper-text">
          Write something about you
         </FormHelperText>
       </FormControl>
-      <FormControl className="col-md-8">
+      <FormControl classNameName="col-md-8">
         <InputLabel htmlFor="password">Password</InputLabel>
         <Input id="password" aria-describedby="my-helper-text" value={props.currentUser.password} />
         <FormHelperText id="my-helper-text">
         Change your password
         </FormHelperText>
       </FormControl>
-      <FormControl className="col-md-8">
+      <FormControl classNameName="col-md-8">
         <InputLabel htmlFor="password">Country</InputLabel>
         <Input id="password" aria-describedby="my-helper-text" value={props.currentUser.country} />
         <FormHelperText id="my-helper-text">
