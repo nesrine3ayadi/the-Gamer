2253e6deb9c980363df3b94e64b7671f3f2f99fe
nesrine3ayadi <nesrine3ayadi@gmail.com>
2020-05-13 10:41:50 +0100

    test commit

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const imgcover = require('../')
-//Create Schema
 
+//Create Schema
+// TEEEEEEEEEEEEEEEEEEEST
 const UserSchema = new Schema({
   username: {
     type: String,
