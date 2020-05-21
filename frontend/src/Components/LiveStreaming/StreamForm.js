20e9246059b9332ef1908689e5b2d0ee657ea70c
nesrine3ayadi <nesrine3ayadi@gmail.com>
2020-05-21 11:23:51 +0100

    ///

--- frontend/src/Components/LiveStreaming/StreamForm.js
+++ frontend/src/Components/LiveStreaming/StreamForm.js
@@ -16,7 +16,7 @@ class StreamForm extends React.Component {
             )
         }
     }
-
+///
     renderInput = ({ input, label, meta }) => {
         const className = `field ${meta.error && meta.touched ? 'error' : ''}`
 
