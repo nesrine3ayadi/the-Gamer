1cd61b342ecf05cb078de0e66eda6576681112c7
chebbi <mohamed.chebbi1@esprit.tn>
2020-05-26 20:28:52 +0100

    c

--- frontend/src/Components/LiveStreaming/ShowStreaming.jsx
+++ frontend/src/Components/LiveStreaming/ShowStreaming.jsx
@@ -5,6 +5,9 @@ import { fetchStream } from "../../Actions/action";
 import { Player } from "video-react";
 import Navbar from "../Navbar/navbar2";
 import Chat from "../Chat/Chat";
+import Axios from "axios"
+import {Link} from "react-router-dom"
+import jwt_decode from "jwt-decode"
 
 class StreamShow extends React.Component {
   constructor(props) {
@@ -41,12 +44,20 @@ class StreamShow extends React.Component {
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
   }
-
+  stopStreaming()
+  {
+    var token = localStorage.getItem("token")
+    var decoded = jwt_decode(token)
+    Axios.put("http://localhost:5000/offLive/"+decoded.id)
+  }
   render() {
     if (!this.props.stream) {
       return <div>Loading..</div>;
     }
 
+    var token = localStorage.getItem("token")
+    var decoded = jwt_decode(token)
+    var id  = decoded.id
     return (
         <div>
 
@@ -61,9 +72,10 @@ class StreamShow extends React.Component {
             <video   ref={this.videoRef} style={{ width: "100%" }}    controls={true}   />
 
             <h5>{this.props.stream.description}</h5>
+    {(id===this.props.match.params.idUser) && <Link to={`/profile/${this.props.match.params.idUser}`}><button onClick={this.stopStreaming}>Stop Streaming </button></Link>} 
           </div>
           <div className="col-md-4 col-sm-12">
-          <Chat  idprofile={this.props.match.params.idUser} />
+           <Chat  idprofile={this.props.match.params.idUser} />
           </div>
         </div>
       </div>   </div>
